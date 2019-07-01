e7d0b46fafa83e08b7f982ad67949c963d007969
Jihwan Kim <eric967712345@gmail.com>
2019-07-01 16:22:23 +0900

    Add login page, timeline template

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
-
-Vue.config.productionTip = false
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <div>
+      <div class="d-inline-block align-top">
+        <img class="" alt="Vue logo" src="../assets/logo.png">
+      </div>
+      <div class="d-inline-block">
+        <form class="form-group form-control-lg" @submit="formSubmit" style="padding-top: 20px">
+          <input class="input-group-text" type="text" name="id" v-model="input.id" placeholder="ID"/>
+          <p></p>
+          <input class="input-group-text" type="text" name="password" v-model="input.password" placeholder="Password"/>
+          <p></p>
+          <button class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        input: {
+          id: "",
+          password: ""
+        }
+      }
+    },
+    methods: {
+      submitLogin: () => {
+
+      }
+    }
+  }
+</script>
+
+<style>
+  p {
+    white-space: nowrap;
+  }
+
+  .home {
+    display: inline-block;
+    text-align: center;
+  }
+</style>
