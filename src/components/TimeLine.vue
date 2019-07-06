196f369da77c8d3e363472623a9d5d6f65809819
Jihwan Kim <eric967712345@gmail.com>
2019-07-06 21:18:36 +0900

    Add TimeLine Card Components

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,76 @@
+<template>
+  <container>
+    <div class="card">
+      <div class="header border-bottom">
+        <img class="card-author-profile" src="../assets/logo.png" alt="">
+        <div class="card-author">author</div>
+      </div>
+      <div class="card-body">
+        <img class="card-img border-bottom" src="../assets/logo.png" alt=""/>
+        <div class="btn-group border-top" role="group">
+          <button type="button" class="btn">like</button>
+          <button type="button" class="btn">comment</button>
+          <button type="button" class="btn">share</button>
+        </div>
+        <div class="card-text p-4">
+          <div class="card-text">Lorem ipsum dolor</div>
+        </div>
+      </div>
+    </div>
+  </container>
+</template>
+
+<script>
+  export default {
+    name: "TimeLine",
+    props: {
+      imgSrc: String,
+      text: String
+    }
+  }
+</script>
+
+<style scoped>
+  .card {
+    width: 500px;
+    height: auto;
+  }
+
+  p.card-text {
+    width: 250px;
+    display: inline-block;
+    border: 1px solid #111;
+    word-wrap: break-word;
+  }
+
+  .btn-group {
+    width: 100%;
+  }
+
+  .btn {
+    border-radius: 0;
+    background-color: white;
+  }
+
+  .header {
+    padding: 10px;
+  }
+
+  .card-body {
+    padding: 0;
+  }
+
+  .card-author-profile {
+    width: 20px;
+    height: auto;
+    float: left;
+    vertical-align: center;
+  }
+
+  .card-author {
+    float: left;
+    vertical-align: center;
+    margin-left: 10px;
+    font-weight: bold;
+  }
+</style>
--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,31 +1,23 @@
 <template>
   <div class="feed">
     <container>
-      <div class="card">
-        <img src="../assets/logo.png" alt=""/>
-        <div class="card-body">
-          <h5 class="card-title">Lorem</h5>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut
-            malesuada.</p>
-          <a href="#" class="btn btn-primary">More</a>
-        </div>
-      </div>
+      <time-line img-src=""
+                 text=""></time-line>
     </container>
   </div>
 </template>
 
 <style>
-
   .feed {
     display: inline-block;
     text-align: center;
     margin-top: 30px;
   }
 
-  p.card-text {
-    width: 250px;
-    display: inline-block;
-    border: 1px solid #111;
-    word-wrap: break-word;
-  }
 </style>
+<script>
+  import TimeLine from "../components/TimeLine";
+  export default {
+    components: {TimeLine}
+  }
+</script>
