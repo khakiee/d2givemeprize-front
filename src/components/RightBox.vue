3640bbfdc047b336717265b56bfdcb78edb30e6d
Jihwan Kim <eric967712345@gmail.com>
2019-07-14 17:42:14 +0900

    Add RightBox-Profile

--- src/components/RightBox.vue
+++ src/components/RightBox.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="r-box">
     <div>
-      <div>
-        recent action
-      </div>
-      <div class="btn-group " role="group">
-        <button type="button" class="btn btn-secondary">ipsum</button>
-        <button type="button" class="btn btn-secondary">lorem</button>
-        <button type="button" class="btn btn-secondary">dolor</button>
+      <div class="profile">
+        <a :href="getMyPageUrl()">
+          <img class="profile-img" src="../assets/logo.png" alt=""/>
+          <div class="profile-namecard">
+            <div class="name">{{userName}}</div>
+            <div class="id">{{userId}}</div>
+          </div>
+        </a>
       </div>
     </div>
     <div>
@@ -25,12 +26,59 @@
 
 <script>
   export default {
-    name: "RightBox"
+    name: "RightBox",
+    props: {
+      userName: String,
+      userId: String,
+      userProfileImg: String
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      getMyPageUrl: function () {
+        return "/user/" + this.userId
+      }
+    },
+    mounted() {
+      this.userName = '김지환'
+      this.userId = 'eric967712'
+    }
   }
 </script>
 
-<style>
+<style scoped>
+  .profile {
+    text-align: left;
+    vertical-align: center;
+    horiz-align: center;
+    padding-bottom: 1rem;
+  }
+
+  .profile-namecard {
+    display: inline-block;
+    vertical-align: center;
+    text-align: left;
+    padding-left: 1rem;
+  }
+
+  .profile-img {
+    width: 3rem;
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  .name {
+    font-weight: bold;
+  }
+
+  .id {
+    font-size: smaller;
+    color: gray;
+  }
+
   .r-box {
     width: 300px;
+    padding: 40px;
   }
 </style>
